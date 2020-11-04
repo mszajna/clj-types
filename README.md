a41f692432901018532c825930f75cc1b4b65bb4
Maciej Szajna <maciej.szajna@polecat.com>
2020-11-04 17:47:12 +0000

    Macroexpansion and other improvements

--- README.md
+++ README.md
@@ -12,15 +12,15 @@ Features:
 ```clojure
 (require '[types :as t])
 
-(t/type 1 (fn [a] (map inc a)))
-; => ([:fn [[:list :int]] [:list :int]])
+(t/type 2 (fn [a] (map inc a)))
+; => (:fn [(every? int?)] (every? int?))
 ```
 
 ## Status & limitations
 
 The type checker is highly experimental.
 
-- supports only `fn` abstraction (`let` is not supported yet) and function application,
+- supports only `fn` abstraction (`let` is not supported yet), function application, and vectors,
 - very few `clojure.core` functions have been annotated (`+`, `inc`, `identity`, `map`),
-- permitted literals are limited to ints,
+- permitted literals are limited to numbers
 - failure to type check sometimes manifests as an infinite loop.
--- src/types.clj
+++ src/types.clj
@@ -1,28 +1,47 @@
 (ns types
-  (:require [clojure.core.logic :as l :refer [defne]]))
+  (:require [clojure.core.logic :as l :refer [defne]]
+            [riddley.walk :refer [macroexpand-all]]))
 
-(def ^:dynamic psub) ; proper (!) subset
-
-(def global-psub-rules (atom [])) ; rules defining psub relation
-(defn reg-psub [r] (swap! global-psub-rules conj r))
+(def
+  ^{:doc "clojure.core.logic relation asserting that smaller is a subset of larger."
+    :dynamic true
+    :arglists '([smaller larger depth])}
+  sub)
 
 (defn interruptible [a]
   (when (Thread/interrupted) (throw (new InterruptedException)))
   a)
 
-(defn make-psub [psub-rules]
-  (l/fne [a b depth] :tabled
-    ([a b d]
-      interruptible
-      (l/trace-lvars "psub" a b)
-      (l/or* (map #(% a b d) psub-rules)))))
+(defn log-sub [a b]
+  (fn [x]
+    (letfn [(get-var [v] (let [val (l/-reify x v)] (if (seqable? val) (doall val) val)))]
+      (println "sub: left =" (get-var a))
+      (println "    right =" (get-var b)))
+    x))
 
-(defn with-rules* [psub-rules f]
-  (binding [psub (make-psub psub-rules)]
-    (doall (distinct (f)))))
+(defn make-sub [sub-rules]
+  (let [reg-sub
+        (l/fne [a b depth]
+          ([a b d]
+            interruptible
+            ; (log-sub a b)
+            (l/or* (map #(% a b d) sub-rules))))]
+    (l/fne [a b depth] :tabled
+      ([x x _])
+      ([x y [_ . d]] (reg-sub x y d))
+      ([x z [_ . d]] (l/fresh [y] (reg-sub x y d) (sub y z d))))))
+
+(defn with-rules* [sub-rules f]
+  (binding [sub (make-sub sub-rules)]
+    (f)))
+
+; Declaring global rules
+
+(def global-sub-rules (atom []))
+(defn rule! [r] (swap! global-sub-rules conj r))
 
 (defmacro with-regs [& body]
- `(with-rules* @global-psub-rules #(do ~@body)))
+ `(with-rules* @global-sub-rules #(do ~@body)))
 
 ; Assigning types to vars
 
@@ -32,52 +51,15 @@
 (defn get-var-type [qsym]
   (-> qsym resolve meta :type))
 
-; Considerations for registering rules
-; psub expresses proper subset - it's important to guarantee
-; transitive relation is bounded. (I think)
+; Function application relation:
 
-; Subset relationship between types a and b, defined as either
-; 'a == 'b, 'a is a known proper subset of 'b, or transitive proper subset of 'b.
-; TODO: decide between soft cut and not.
-; Soft cut has a clear cut stop condition but is it guaranteed to find matches?
-; This seems to fail when defne but should evaluate to :nothing
-; I seem to be able to get good results with a limited number of matches,
-; assuming I can the ordering right. But ordering seems to only work in case
-; of defna that doesn't work in all cases..
-(comment
- ; this fails to get any matches when sub is defined with defna
- (types/with-regs (l/run 1 [q] (types/sub q `number?) (types/sub :nothing q) (types/sub q `int?))))
-; Update: I got the conda business wrong!
-; It goes through each alternative evaluating the first clause. First that succeeds locks
-; conda in, but remaining clauses still have to succeed themselves.
-; No further alternatives are considered.
-; (conda [succeed fail] [succeed]) for instance, fails.
-; And so does:
-; (l/run 1 [q]
-;   (l/conda [(l/== q 1)] [(l/== q 2)])
-;   (l/== q 2))
-; Which looks like a deal breaker
+(defne app [f args e depth]
+  ([[:fn [a] r] [p] r d] (sub p a d))
+  ([[:fn [a1 a2] r] [p1 p2] r d] (sub p1 a1 d) (sub p2 a2 d)))
+; TODO: more arities
 
-; Core relations
-
-; (defn sub [a b depth]
-;   (l/conde
-;     [(l/== a b)]
-;     [(l/!= a b) (psub a b)]
-;     [(l/fresh [t] (l/distincto [a t b]) (psub a t) (sub t b))]))
-
-(defne sub [a b depth]
-  ([x x _])
-  ([x y [_ . d]] (psub x y d))
-  ([x z [_ . d]] (l/fresh [y] (psub x y d) (sub y z d))))
-
-(def d (range 5))
-
-(defne app [f args e]
-  ([[:fn [a] r] [p] r] (sub p a d))
-  ([[:fn [a1 a2] r] [p1 p2] r] (sub p1 a1 d) (sub p2 a2 d)))
-
-; The 'type' macro
+; ##### 'type' macro ######
+; #########################
 
 (defn var-name [env sym]
   (when-let [v (and (symbol? sym) (resolve env sym))]
@@ -85,175 +67,132 @@
           nsp (.getName ^clojure.lang.Namespace (:ns (meta v)))]
       (symbol (name nsp) (name nm)))))
 
-(defn teval [env x e]
+(defn teval [env x result depth]
   (let [call (when (seq? x) (first x))]
     (cond
       (or (number? x) (string? x))
-     `(l/== ~x ~e)
+     `(l/== ~result ~x)
 
-      (= 'fn call)
-      (let [[_ args & body] x
+      (= 'fn* call)
+      (let [[_ [args & body]] x ; TODO: expand support to multi-arities
             nenv (gensym "nenv")
             r (gensym "r")]
        `(l/fresh [~@args ~r]
-          (l/== ~e [:fn [~@args] ~r])
-         ~(teval (merge env (zipmap args (repeat true))) `(do ~@body) r)))
+          (l/== ~result (list :fn [~@args] ~r))
+         ~(teval (merge env (zipmap args (repeat true))) `(do ~@body) r depth)))
       
       (= 'do call)
       (let [xs (rest x)]
         (cond
-          (empty? xs) `(l/== nil ~e)
-          (empty? (rest xs)) (teval env (first xs) e)
+          (empty? xs) `(l/== ~result nil)
+          (empty? (rest xs)) (teval env (first xs) result depth)
           :else
           (let [syms (mapv (fn [_] (gensym "_")) (butlast xs))]
            `(l/fresh ~syms
-             ~@(map #(teval env %1 %2) (butlast xs) syms)
-             ~(teval env (last xs) e)))))
+             ~@(map #(teval env %1 %2 depth) (butlast xs) syms)
+             ~(teval env (last xs) result depth)))))
 
       (seq? x)
       (let [f (gensym "f")
             syms (mapv (fn [_] (gensym "e")) (rest x))]
        `(l/fresh [~f ~@syms]
-         ~(teval env (first x) f)
-         ~@(map #(teval env %1 %2) (rest x) syms)
-          (app ~f ~syms ~e)))
+         ~(teval env (first x) f depth)
+         ~@(map #(teval env %1 %2 depth) (rest x) syms)
+          (app ~f ~syms ~result ~depth)))
 
       (vector? x)
       (let [syms (mapv (fn [_] (gensym "e")) x)]
        `(l/fresh [~@syms]
-         ~@(map #(teval env %1 %2) x syms)
-          (l/== ~e [:vector ~@syms])))
+         ~@(map #(teval env %1 %2 depth) x syms)
+          (l/== ~result [~@syms])))
 
       (symbol? x)
       (if-let [name (var-name env x)]
-       `((get-var-type (quote ~name)) ~e)
-       `(l/== ~x ~e))
+       `((get-var-type (quote ~name)) ~result)
+       `(l/== ~result ~x))
 
       :else (throw (new IllegalArgumentException (str "wtf is " x))))))
 
-(defmacro type [n exp]
+(defmacro type [depth exp]
   (let [e (gensym "e")]
    `(with-regs
-      (l/run ~n [~e]
-       ~(teval &env exp e)))))
-
-; TODO: teval above tries to satisfy all expressions at once. While this
-; is cool in principle, ideally I should be able to follow the execution order
-; and pinpoint the exact place that causes trouble.
+      (first
+        (l/run 1 [~e]
+         ~(teval &env (macroexpand-all exp) e `(range ~depth)))))))
 
 ; ######## RULES ########################
 ; #######################################
 
-; Rules: top and bottom
-; TODO: top corresponds to unbound lvar
-; TODO: bottom corresponds to logic failure
-; Q: Do I even need these rules?
-
-; (defne top-type [a b] ([a :any] (l/!= a :any)))
-; (reg-psub top-type)
-
-; (defne bottom-type [a b] ([:nothing b] (l/!= :nothing b)))
-; (reg-psub bottom-type)
-
 ; Rules: numbers
 
 (defne int-literal [a b _] ([x `int? _] (l/pred x int?)))
-(reg-psub int-literal)
+(rule! int-literal)
 
 (defne int-is-num [a b d] ([`int? `number? d]))
-(reg-psub int-is-num)
-(comment (reg (int? %) => (number? %)))
+(rule! int-is-num)
 
 (defne float-literal [a b _] ([x `float? _] (l/pred x float?)))
-(reg-psub float-literal)
+(rule! float-literal)
 
 (defne float-is-num [a b _] ([`float? `number? _]))
-(reg-psub float-is-num)
+(rule! float-is-num)
 
 (defne ratio-literal [a b _] ([x `ratio? _] (l/pred x ratio?)))
-(reg-psub ratio-literal)
+(rule! ratio-literal)
 
 (defne ratio-is-num [a b _] ([`ratio? `number? _]))
-(reg-psub ratio-is-num)
+(rule! ratio-is-num)
 
 ; Rules: functions
 
 (defne sub-args [a b depth max-args]
  ([[] [] _ _])
- ([['& xs] ['& ys] d _] (sub xs ys d)) 
- ([[x . xs] [x . ys] d [_ . a]] (sub-args xs ys d a))
+ ([['& xs] ['& ys] d _] (sub xs ys d))
  ([[x . xs] [y . ys] d [_ . a]]
   (sub y x d)
   (sub-args xs ys d a)))
 ; TODO: (= [:fn [`int? & `int?] `int?]  [:fn [& `int?] `int?])
-; TOOD: pattern matching turns vectors into lists breaking the convention
 
 (def max-args 20) ; maximum number of arguments supported by Clojure functions
 
 (defne function-n-arity [a b depth]
- ([[:fn largs lr] [:fn rargs rr] d]
+ ([[:fn largs lr . nil] [:fn rargs rr . nil] d]
   (sub-args largs rargs d (range max-args))
   (sub lr rr d)))
-(reg-psub function-n-arity)
-(comment (reg (fn largs lret) (=> rargs largs) (=> lret rret) => (fn rargs rret)))
+(rule! function-n-arity)
 
 ; Rules: lists
 
 (defne homogeneous-collection [a b depth]
- ([[`every? x '%] [`every? y '%] d] (sub x y d)))
-(reg-psub homogeneous-collection)
-(comment (reg (every? a %) (=> a b) => (every? b %)))
-
-; Considerations:
-; Up to homogeneous-list rule above, the space of types was limited and it was
-; possible to traverse it all. Therefore, it was possible to disprove an expression.
-; The rule above introduces infinite number of new types with growing nested lists.
-; When given two incompatible types (sub `int? [`every? a]), he logic engine tries to
-; traverse it all and obviously gets caught in an infinite loop.
-
-; It is becoming obvious that being able to disprove an expession is as important as to
-; prove it. Java has a type hierarchy where `int? and `every? will never unify. I wanted
-; to avoid it but can I?
-
-; (psub b a) implies !(sub a b) but this is a limited solution. In many cases the two
-; have no declared relationship.
-
-; This loops forever:
-; (types/with-regs (l/run 1 [a] (l/fresh [x] (types/psub a [`every? x]) (types/psub `number? a))))))
-; The engine isn't able to figure out there is no such x, such that (psub `number? [`every? x])
-; But then (l/run 1 [a] (types/psub `number? [`every? a]))
-; completes with no problems?
-; And so does (l/run 1 [a] (l/fresh [x] (types/psub `number? a) (types/psub a [`every? x])))
+ ([[`every? x '% . nil] [`every? y '% . nil] d] (sub x y d)))
+(rule! homogeneous-collection)
 
 ; Rules: vectors / tuples
 
-(defne every-sub [coll p depth]
+(defne every-sub [coll pred depth]
  ([[] _ d])
  ([[x . xs] p d]
   (sub x p d)
   (every-sub xs p d)))
 
-(defne vector-as-every [a b depth]
- ([[:vector . xs] [`every? p '%] d]
-  (l/project [xs] (every-sub xs p d))))
-(reg-psub vector-as-every)
+(defn vector-as-every [a b depth]
+  (l/pred a vector?)
+  (l/matche [b]
+   ([[`every? p '% . nil]] (every-sub a p depth))))
+(rule! vector-as-every)
 
 ; Rules: maps & structural
 
 ; Clojure core functions
 
-(set-type! #'+ (l/fne [_] ([[:fn [`int? `int?] `int?]]) ([[:fn [`number? `number?] `number?]])))
-(set-type! #'/ (l/fne [_] ([[:fn [`number?] `number?]]) ([[:fn [`number? `number?] `number?]])))
-(set-type! #'inc (l/fne [_] ([[:fn [`int?] `int?]])))
+(set-type! #'+ (l/fne [_] ([[:fn [`int? `int?] `int? . nil]]) ([[:fn [`number? `number?] `number? . nil]])))
+(set-type! #'/ (l/fne [_] ([[:fn [`number?] `number? . nil]]) ([[:fn [`number? `number?] `number? . nil]])))
+(set-type! #'inc (l/fne [_] ([[:fn [`int?] `int? . nil]])))
 
-(set-type! #'identity (l/fne [_] ([[:fn [a] a]])))
-(set-type! #'constantly (l/fne [_] ([[:fn [a] [:fn [_] a]]])))
+(set-type! #'identity (l/fne [_] ([[:fn [a] a . nil]])))
+(set-type! #'constantly (l/fne [_] ([[:fn [a] [:fn [_] a . nil] . nil]])))
 
-(set-type! #'map (l/fne [_] ([[:fn [[:fn [a] b] [`every? a '%]] [`every? b '%]]])))
-(set-type! #'first (l/fne [_] ([[:fn [[`every? a '%]] a]]) ([[:fn [[:vector a . _]] a]])))
-(set-type! #'second (l/fne [_] ([[:fn [[:vector _ . a . _]] a]])))
-(set-type! #'rest (l/fne [_] ([[:fn [[`every? a '%]] [`every? a '%]]])))
-
-(comment
-  ; Ideally, the syntax would be something along the lines of:
-  (forall [a b] `(fn [(every? a %) (fn [a] b)] (every? b %))))
+(set-type! #'map (l/fne [_] ([[:fn [[:fn [a] b . nil] [`every? a '% . nil]] [`every? b '% . nil] . nil]])))
+(set-type! #'first (l/fne [_] ([[:fn [[`every? a '% . nil]] a . nil]]) ([[:fn [[:vector a . _]] a . nil]])))
+(set-type! #'second (l/fne [_] ([[:fn [[:vector _ . a . _]] a . nil]])))
+(set-type! #'rest (l/fne [_] ([[:fn [[`every? a '% . nil]] [`every? a '% . nil] . nil]])))
